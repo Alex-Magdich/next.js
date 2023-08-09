984db0f0cfd3da500899dcae7aa617dc104e46f0
Alex-Magdich <alexkotorij@gmail.com>
2023-08-09 23:55:12 +0300

    fix

--- app/constants.ts
+++ app/constants.ts
@@ -1,15 +1,9 @@
 export enum PAGES {
-    // eslint-disable-next-line no-unused-vars
     HOME = '/',
-    // eslint-disable-next-line no-unused-vars
     FEATURES = "/features",
-    // eslint-disable-next-line no-unused-vars
     BLOG = "/blog",
-    // eslint-disable-next-line no-unused-vars
     SHOP = "/shop",
-    // eslint-disable-next-line no-unused-vars
     ABOUT = "/about",
-    // eslint-disable-next-line no-unused-vars
     CONTACT_US = "/contact-us",
 }
 
