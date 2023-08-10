24c3c4da80f4e0a3c9d229feeb67e68edb405cee
Alex-Magdich <alexkotorij@gmail.com>
2023-08-10 17:32:07 +0300

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+import {PAGES} from "@/app/constants";
+
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+        <div className="container text-center">
+            <h1 className='mt-base mb-base'>Home</h1>
+            <p className='mb-base2x'>content will be soon</p>
+            <Link href={PAGES.CONTACT_US} className='button mb-base2x'>GO TO CONTACT PAGE</Link>
+        </div>
     </main>
   )
 }
